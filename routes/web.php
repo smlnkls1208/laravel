b25364b6de07b741188ffafa2e2936f70f18318f
smlnkls1208 <al89994950059@gmail.com>
2025-11-01 12:22:08 +0700

    Маршруты ч.2

--- routes/web.php
+++ routes/web.php
@@ -6,59 +6,41 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-/*Route::get('/', function () {
-    return '<h1>Hello world!</h1>';
+Route::get('/', [\App\Http\Controllers\MainController::class, 'index']);
+
+/*Route::get('/admin', function() {
+    return 'Admin main page';
+});
+
+Route::get('/admin/posts', function() {
+    return 'Admin posts page';
+});
+
+Route::get('/admin/posts/{id}', function($id) {
+    return "Admin post {$id}";
 });*/
 
-/*Route::get('/', function() {
-    return view('hi', ['title' => 'Main page']);
-});*/
+Route::prefix('admin') -> group(function () {
+    Route::get('/', function () {
+        return 'Admin main page';
+    });
 
-Route::view('/', 'test/hi', ['title' => 'Main page']);
+    Route::get('//posts', function () {
+        return 'Admin posts page';
+    });
 
-/*Route::get('/posts/{id?}', function($id = 1) {
-    return "Posts ID: {$id}";
+    Route::get('/posts/{id}', function ($id) {
+        return "Admin post {$id}";
+    });
 });
 
-Route::get('/posts/{id}/comments/{comment}', function($id, $comment_id) {
-    return "Posts ID: {$id}, Comment ID: {$comment_id}";
-});*/
-
-/*Route::get('/posts/{id}', function($id) {
-    return "Posts ID: {$id}";
-})->where(['id' => '[\d]+']);*/
-
-Route::get('/posts', function() {
-    return "Posts page";
+Route::fallback(function () {
+    abort(404, '404 - Page not found');
+    return response()->json(['answer' => '404 - Page not found'], 404);
+    return response('404 - Page not found', 404);
+    return '404 - Page not found';
 });
 
-Route::get('/posts/{id}', function($id) {
-    return "Posts ID: {$id}";
+Route::fallback(function () {
+    abort(404, '404 - Page not found');
 });
-
-Route::get('/posts/contact', function() {
-    return "post CONTACT";
-});
-
-Route::get('/posts/{slug}', function($slug) {
-    return "Posts SLUG: {$slug}";
-});
-
-Route::get('/search/{search}', function($search) {
-    return "Searching: {$search}";
-})->where(['search' => '.*']); //данный метод уже не работает на 12 версии ларавел
-
-
-Route::post('posts', function() {
-    return 'Store posts';
-})->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
-
-/*Route::match(['get', 'post'], '/get-post', function() {
-    return 'Hello from GET|POST';
-})->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);*/
-
-Route::any('/get-post', function() {
-    return 'Hello from GET|POST';
-})->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
-
-Route::redirect('/here', 'get-post', 301);
