ba4223fda46a361dc4ca676f1052005fc470eb52
smlnkls1208 <al89994950059@gmail.com>
2025-11-02 13:21:49 +0700

    шаблонизаторы blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
 
     public function index(): \Illuminate\Contracts\View\View
     {
-        return view('home.index', ['title2' => 'Home page', 'description' => 'Test desc', 'test' => 'test value']);
+        $users = json_decode(file_get_contents('https://jsonplaceholder.typicode.com/users'), true);
+//        $users = Http::get('https://jsonplaceholder.typicode.com/users')->json();
+//        dump($users);
+
+        return view('home.index', compact('users'));
     }
 
     public function test()
