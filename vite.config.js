ad243af6b96ee69fc5224fa979a445014811d462
smlnkls1208 <al89994950059@gmail.com>
2025-11-02 14:50:08 +0700

    сборщик vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/bootstrap/bootstrap.css', 'resources/css/main.css', 'resources/bootstrap/bootstrap.bundle.js'],
             refresh: true,
         }),
         tailwindcss(),
