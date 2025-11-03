5c5c8e1efab414b8ca0972a2f653e2f63d6f99cc
smlnkls1208 <al89994950059@gmail.com>
2025-11-03 23:46:18 +0700

    база ORM, выборка и модели

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+/*Route::get('countries', function () {
+    return \App\Models\Country::query()
+        ->where("Population", '>', 100000000)
+        ->orderBy('Population', 'desc')
+        ->limit(5)
+        ->get(['Code', 'Name', 'Population']);
+});*/
