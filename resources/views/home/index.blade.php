27d262b08da47722cf1bd8a7163be3faa33a5db2
smlnkls1208 <al89994950059@gmail.com>
2025-11-08 17:49:34 +0700

    начало валидации

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,34 +10,6 @@
 @section('content')
     <h1>Home page</h1>
 
-    @php
-        /**
-        * @var Collection $products
-        * Post $post
-        */
-    @endphp
-{{--    @foreach($products->chunk(3) as $chunk)
-        <div class="row my-3">
-            @foreach($chunk as $product)
-                <div class="col-md-4">
-                    {{ $product['title'] }}
-                </div>
-            @endforeach
-        </div>
-    @endforeach--}}
-
-{{--    @foreach($posts as $post)
-        {{ $post->title }} | {{ $post->isPublished() }}<br>
-    @endforeach
-    <hr>
-    @foreach($posts2 as $post)
-        {{ $post->title }}<br>
-    @endforeach
-    <hr>
-    @foreach($posts3 as $post)
-        {{ $post->title }}<br>
-    @endforeach
-    <hr>--}}
 
 @endsection
 
