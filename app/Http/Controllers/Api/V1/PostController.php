e99cd1d134ee2af1515b5ff1aef4ec909e520c99
smlnkls1208 <al89994950059@gmail.com>
2025-11-09 17:25:18 +0700

    эндпоинты для постов

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        return Post::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        if ($post->id == 3) {
+            return response()->json([
+                'message' => 'Forbidden'
+            ], 403);
+        }
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        $post->update($request->all());
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json([
+            'message' => 'Post removed'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,8 @@ Route::get('/v1/categories', [CategoryController::class, 'index']);
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('posts', PostController::class);
+
 });
+
+
