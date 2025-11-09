b717bd8a85d8054c9db62b6562f472e335253d25
smlnkls1208 <al89994950059@gmail.com>
2025-11-09 13:48:19 +0700

    создана БД, модель, контроллер, маршруты для категорий

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/v1/categories', [CategoryController::class, 'index']);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
