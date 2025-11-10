711fbabca31265fc1603cf3c75b8787309dc1ae1
smlnkls1208 <al89994950059@gmail.com>
2025-11-10 16:27:33 +0700

    регистрация, авторизация, токены

---
+++ app/Http/Resources/V1/PostResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Route;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'categoryId' => $this->category_id,
+            'categoryName' => $this->category->title,
+            'content' => $this->when(Route::currentRouteName() =='posts.show', $this->content),
+            'created' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
+            'updated' => Carbon::parse($this->updated_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('v1')->middleware('throttle:api')->group(function () {
+    Route::post('register', [App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+    Route::post('login', [App\Http\Controllers\Api\V1\AuthController::class, 'login']);
+});
+
 
 Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function () {
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('posts', PostController::class);
+
+    Route::get('logout', [App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
 
 });
 
